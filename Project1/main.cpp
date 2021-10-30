5c970a09994cce4ccba23650d1ed7d93baa14551
Alexander Karevo <karevo1@mail.ru>
2021-10-30 18:13:29 +0300

    One Ship On The Screen

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,6 +6,11 @@ class Ship
 
 public:
 	Ship(int ship_length);
+	short getXStart();
+	short getXEnd();
+	short getYStart();
+	short getYEnd();
+
 };
 Ship::Ship(int ship_length)
 {
@@ -15,13 +20,46 @@ Ship::Ship(int ship_length)
 	y_start = y_end = 5;
 }
 
+short Ship::getXStart()
+{
+	return x_start-1;
+}
+
+short Ship::getXEnd()
+{
+	return x_end-1;
+}
+
+short Ship::getYStart()
+{
+	return y_start-1;
+}
+
+short Ship::getYEnd()
+{
+	return y_end-1;
+}
+
 class Field
 {
 	char field_image[10][10];
 
 public:
+	Field();
 	void printField();
+	void setShip(Ship ship);
 };
+
+Field::Field()
+{
+	for (size_t i = 0; i < 10; i++)
+	{
+		for (size_t j = 0; j < 10; j++)
+		{
+			field_image[i][j] = '+';
+		}
+	}
+}
 
 void Field::printField()
 {
@@ -29,13 +67,30 @@ void Field::printField()
 	{
 		for (size_t j = 0; j < 10; j++)
 		{
-			std::cout << field_image[i][j];
+			std::cout << field_image[j][i] << " ";
 		}
 		std::cout << std::endl;
 	}
 }
 
+void Field::setShip(Ship ship)
+{
+	for (short i = ship.getXStart(); i <= ship.getXEnd(); i++)
+	{
+		for (short j = ship.getYStart(); j <= ship.getYEnd(); j++)
+		{
+			field_image[i][j] = 'S';
+		}
+	}
+}
+
+
 int main()
 {
-
+	Field left_field;
+	left_field.printField();
+	Ship ship_x4 (4);
+	left_field.setShip(ship_x4);
+	std::cout << std::endl;
+	left_field.printField();
 }
